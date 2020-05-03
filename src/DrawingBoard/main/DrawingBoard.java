6d5464356ef18bc11e5c458549c55e4a31caa0c5
llleyelll <chida.fua@gmail.com>
2020-05-03 15:12:00 +0700

    Add Drawing board package

---
+++ src/DrawingBoard/main/DrawingBoard.java
@@ -0,0 +1,136 @@
+package DrawingBoard.main;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JPanel;
+import DrawingBoard.objects.*;
+
+public class DrawingBoard extends JPanel {
+
+	private MouseAdapter mouseAdapter; 
+	private List<GObject> gObjects;
+	private GObject target;
+	
+	private int gridSize = 10;
+	
+	public DrawingBoard() {
+		gObjects = new ArrayList<GObject>();
+		mouseAdapter = new MAdapter();
+		addMouseListener(mouseAdapter);
+		addMouseMotionListener(mouseAdapter);
+		setPreferredSize(new Dimension(800, 600));
+	}
+	
+	public void addGObject(GObject gObject) {
+		gObjects.add(gObject);
+		repaint();
+	}
+	
+	public void groupAll() {
+		CompositeGObject groupObject = new CompositeGObject();
+
+		for (GObject child : gObjects) {
+			if (child instanceof CompositeGObject) {
+				groupObject = (CompositeGObject) child;
+			} else {
+				groupObject.add(child);
+			}
+		}
+
+		groupObject.recalculateRegion();
+		gObjects.clear();
+		gObjects.add(groupObject);
+		repaint();
+	}
+
+	public void deleteSelected() {
+		gObjects.remove(target);
+		repaint();
+	}
+	
+	public void clear() {
+		gObjects.clear();
+		repaint();;
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		paintBackground(g);
+		paintGrids(g);
+		paintObjects(g);
+	}
+
+	private void paintBackground(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(0, 0, getWidth(), getHeight());
+	}
+
+	private void paintGrids(Graphics g) {
+		g.setColor(Color.lightGray);
+		int gridCountX = getWidth() / gridSize;
+		int gridCountY = getHeight() / gridSize;
+		for (int i = 0; i < gridCountX; i++) {
+			g.drawLine(gridSize * i, 0, gridSize * i, getHeight());
+		}
+		for (int i = 0; i < gridCountY; i++) {
+			g.drawLine(0, gridSize * i, getWidth(), gridSize * i);
+		}
+	}
+
+	private void paintObjects(Graphics g) {
+		for (GObject child : gObjects) {
+			child.paint(g);
+		}
+	}
+
+	class MAdapter extends MouseAdapter {
+
+		private int x;
+		private int y;
+		
+		private void deselectAll() {
+			for (GObject child : gObjects) {
+				child.deselected();
+			}
+
+			target = null;
+		}
+		
+		@Override
+		public void mousePressed(MouseEvent e) {
+			x = e.getX();
+			y = e.getY();
+			boolean mouseHit = false;
+
+			deselectAll();
+			for (GObject child : gObjects) {
+				if (child.pointerHit(x, y)) {
+					target = child;
+					mouseHit = true;
+					break;
+				}
+			}
+			if (mouseHit) {target.selected();}
+			repaint();
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			if (target != null) {
+				// find distance for move
+				int dX = e.getX() - x;
+				int dY = e.getY() - y;
+
+				target.move(dX, dY);
+				repaint();
+
+				x = e.getX();
+				y = e.getY();
+			}
+		}
+	}
+	
+}
---
+++ src/DrawingBoard/objects/CompositeGObject.java
@@ -0,0 +1,68 @@
+package DrawingBoard.objects;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositeGObject extends GObject {
+
+	private List<GObject> gObjects;
+
+	public CompositeGObject() {
+		super(0, 0, 0, 0);
+		gObjects = new ArrayList<GObject>();
+	}
+
+	public void add(GObject gObject) {
+		gObjects.add(gObject);
+	}
+
+	public void remove(GObject gObject) {
+		gObjects.remove(gObject);
+	}
+
+	@Override
+	public void move(int dX, int dY) {
+		x += dX;
+		y += dY;
+
+		for (GObject child : gObjects) {
+			child.move(dX, dY);
+		}
+	}
+
+	public void recalculateRegion() {
+		GObject firstChild = gObjects.get(0);
+		int maxX = 0;
+		int maxY = 0;
+		int minX = firstChild.x;
+		int minY = firstChild.y;
+
+		for (GObject child : gObjects) {
+			if (child.x + child.width > maxX) maxX = child.x + child.width;
+			if (child.x < minX) minX = child.x;
+			if (child.y + child.height > maxY) maxY = child.y + child.height;
+			if (child.y < minY) minY = child.y;
+		}
+
+		this.x = minX;
+		this.y = minY;
+		this.width = maxX - minX;
+		this.height = maxY - minY;
+	}
+
+	@Override
+	public void paintObject(Graphics g) {
+		for (GObject child : gObjects) {
+			child.paintObject(g);
+		}
+	}
+
+	@Override
+	public void paintLabel(Graphics g) {
+		g.setColor(Color.black);
+		g.drawString("Group", x, y + height + 15);
+	}
+	
+}
